1d605d59eca5925d1acad3648307fc2e4ab57b39
Prathamesh <prathamesh.v@rapsystechnologies.com>
2025-10-08 15:17:10 +0530

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals'),
+  {
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'no-console': 'warn',
+      'no-unused-vars': 'error',
+    },
+  },
+];
+
+export default eslintConfig;
